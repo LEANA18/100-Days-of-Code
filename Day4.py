8d0eb2a21d6a6eff6fa9e1382f191b9722ed24e8
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-07-12 07:05:47 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,5 +1,5 @@
 #Print Function
-The included code stub will read an integer, , from STDIN.
+The included code stub will read an integer,n, from STDIN.
 
 Without using any string methods, try to print the following:
 123...n
