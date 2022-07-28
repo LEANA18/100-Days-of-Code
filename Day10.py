c5807782a93946c8b2641d0764727b0949d13550
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-07-28 11:00:17 +0530

    Create Day10.py

---
+++ Day10.py
@@ -0,0 +1,12 @@
+Chef considers the climate HOT if the temperature is above 2020, otherwise he considers it COLD. 
+You are given the temperature C, find whether the climate is HOT or COLD.
+
+x=int(input())
+for i in range(1,x+1):
+    C=int(input())
+    if C<=20:
+        print("COLD")
+    else:
+        print("HOT")
+
+
