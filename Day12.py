7b8efb1a591ed9c1ef1a03d324b158f1f67cb6a9
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-08-03 12:06:23 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,13 @@
+#Math 
+#Triangle Quest
+You are given a positive integer n. Print a numerical triangle of height n-1 like the one below:
+1
+22
+333
+4444
+55555
+Can you do it using only arithmetic operations, a single for loop and print statement?
+
+
+for i in range(1,int(input())): 
+    print((pow(10,i)//9)*i)
