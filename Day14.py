bf278bf46947c9ea6b295b83a50050a38daf2ec1
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-08-06 11:21:06 +0530

    Create Day14.py

---
+++ Day14.py
@@ -0,0 +1,27 @@
+Alice buys a toy with a selling price of 100 rupees. 
+There is a discount of x percent on the toy. 
+Find the amount Alice needs to pay for it.
+
+
+
+n=int(input())
+sellingprice=100
+for i in range(n):
+    discount=int(input())
+    amount=sellingprice-discount
+    print(amount)
+    
+Input
+*****
+4
+5
+9
+11
+21
+Output
+******
+95
+91
+89
+79
+
