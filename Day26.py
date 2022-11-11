0ffac0b320c2ff6497be8b02027a60b132e0457b
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-11-11 23:12:19 +0530

    Create Day26.py

---
+++ Day26.py
@@ -0,0 +1,82 @@
+#Strings
+#Alphabet Rangoli
+
+You are given an integer N  
+Your task is to print an alphabet rangoli of size N 
+(Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:
+
+#size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+
+The center of the rangoli has the first alphabet letter a, and the boundary has the N th alphabet letter (in alphabetical order).
+
+def print_rangoli(size):
+    l1=list(map(chr,range(97,123)))
+    x=l1[n-1::-1]+l1[1:n]
+    m=len('-'.join(x))
+    for i in range(1,n):
+        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
+    for i in range(n,0,-1):
+        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
+    
+    
+Input
+*****
+ 5
+    
+Output
+******
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
