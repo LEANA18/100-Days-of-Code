fa250f33f253f70830f5cd1d8de2388a9c2a448c
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-02-07 12:03:46 +0530

    Create Day37.py

---
+++ Day37.py
@@ -0,0 +1,42 @@
+#30 Days of Code
+#Day 0:Hello, World.
+
+Task
+****
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
+and finally print the value of your variable on a second line. 
+
+Input Format
+************
+
+A single line of text denoting inputString (the variable whose contents must be printed).
+
+Output Format
+*************
+Print Hello, World. on the first line, and the contents of inputString  on the second line.
+
+Code
+****
+
+input_string = input()
+print('Hello, World.')
+print(input_string)
+
+Input 1
+*******
+Welcome to 30 Days of Code!
+
+Output 1
+********
+Hello, World. 
+Welcome to 30 Days of Code!
+
+Input 2
+*******
+HackerRank is the best!
+
+Output 2
+********
+Hello, World.
+HackerRank is the best!
+
