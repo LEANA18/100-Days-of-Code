ba814091f7b98fd5cded5fcad4e4c698a728a135
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-03-08 22:00:19 +0530

    Create Day46.js

---
+++ Day46.js
@@ -0,0 +1,87 @@
+#JavaScript
+#30DaysOfCode
+#Day 1
+#Arithmetic Operators
+
+Task
+****
+
+Complete the following functions in the editor below:
+
+getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+
+Program
+*******
+  'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    
+    area=length*width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter=2*(length+width);
+    
+    return perimeter;
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
+
+Input
+*****
+  3
+4.5
+
+Output
+*****
+13.5
+15
