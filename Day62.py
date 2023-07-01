430bbeae1c4df124d4cddb64f3f08da5484fa260
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-07-01 07:02:55 +0530

    Create Day62.py

---
+++ Day62.py
@@ -0,0 +1,34 @@
+#Algorithms
+#Warm Up
+#Solve Me First
+
+Task
+****
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+*******
+  a=7
+  b=3
+
+Return 10 .
+Program
+*******
+  def solveMeFirst(a,b):
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
+
+Input
+*****
+a = 2
+b = 3
+
+Output
+******
+5
