899e1bc71e1a99eabeacc9253fecb2e95527b1c1
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-07-28 22:09:58 +0530

    Create Day64(2).sh

---
+++ Day64(2).sh
@@ -0,0 +1,22 @@
+#LoopingandSkipping
+
+Task
+****
+Your task is to use for loops to display only odd natural numbers from 1 to 99.
+
+for i in {1..99..2}
+do
+    echo $i
+done
+
+Output
+******
+1
+3
+5
+.
+.
+.
+.
+.
+99  
