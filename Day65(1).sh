6cc09295999f1c655404d94974d84ad0b7454ce8
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-07-29 14:44:23 +0530

    Create Day65(1).sh

---
+++ Day65(1).sh
@@ -0,0 +1,16 @@
+#PersonalisedEcho
+
+Task
+****
+Write a Bash script which accepts name  as input and displays the greeting "Welcome (name)"
+
+read name
+echo "Welcome $name"
+
+Input
+*****
+Dan
+
+Output
+******
+Welcome Dan 
