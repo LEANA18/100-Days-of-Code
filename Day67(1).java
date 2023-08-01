f63f62100776e0971808a9da90739cf8f360fd2b
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-08-01 07:00:43 +0530

    Create Day67(1).java

---
+++ Day67(1).java
@@ -0,0 +1,49 @@
+#Java Loops 1
+
+
+Task
+****
+Given an integer N print its first 10 multiples. Each multiple N*i (where 1<=i<=10) should be printed on a new line in the form: N x i = result.
+
+Program
+*******
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    private static final Scanner sc= new Scanner(System.in);
+    
+    public static void main(String[] args) throws IOException {
+      int N=sc.nextInt();
+      for(int i=1;i<=10;i++)
+        {
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+
+        sc.close();
+      }
+}
+
+Input
+*****
+2
+
+Output
+******
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
